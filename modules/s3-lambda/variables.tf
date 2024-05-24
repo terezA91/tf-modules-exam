517f9210d3eea9124124394c2b8dcd3cd18874df
Tereza <bisharyant>
2024-05-24 07:35:51 +0400

    moving code on specific module

---
+++ modules/s3-lambda/variables.tf
@@ -0,0 +1,16 @@
+variable "source_dir" {
+	type        = string
+	description = "Directory of target file containing Lambda function"
+	default     = "python"
+}
+
+variable "func_name" {
+	type = string
+	description = "Name of Lambda function"
+} 
+
+variable "runtime_language" {
+	type        = string
+	description = "Name and version of Lambda runtime language"
+	default     = "python3.8"
+}
