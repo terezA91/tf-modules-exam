c00607b115ab104542636388736fc7eb37fb3e35
Tereza <bisharyant>
2024-05-24 09:09:12 +0400

    Add particual s3 module

---
+++ modules/s3/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+	region = "eu-north-1"
+	profile = default
+}
+
+resource "aws_s3_bucket" "b1" {
+	bucket = var.bucket_name
+	force_destroy = var.destroy_bucket
+	
+	tags = {
+		Name = "Custom bucket"
+	}
+}
+
+resource "aws_s3_bucket_public_access_block" "exam" {
+	block_public_acls = true  //default value
+	block_public_policy = var.s3_block_public_policy
+	ignore_public_acls = true  //default value
+	restrict_public_buckets = var.s3_restrict_public_buckets
+}
+
+resource "aws_s3_bucket_versioning" "bv" {
+	bucket = aws_s3_bucket.b1.id
+	versioning_configuration {
+		status = var.enable_versioning[0]
+	}
+}
---
+++ modules/s3/variables.tf
@@ -0,0 +1,31 @@
+variable "bucket_name" {
+	type = string
+	description = "Name of the s3 bucket"
+	default = "My S3 bucket"
+}
+
+variable "destroy_bucket" {
+	type = bool
+	description = "Delete an bucket regardless of the presence of the object"
+	default = true
+}
+
+variable "enable_versioning" {
+	type = list
+	description = "Enable bucket versioning or not"
+	default = ["Disabled", "Enabled", "Suspended"]
+}
+
+variable "s3_block_public_policy" {
+	type = bool
+	description = "Block `public_policy` in s3 or not"
+	default = false
+}
+
+variable "s3_restrict_public_buckets" {
+	type = bool
+	description = "Restrict public buckets in s3 or not"
+	default = false
+}
+
+
