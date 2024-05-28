6fd1f5cb4d2c9f10bf111e1966dfddf4f899b195
Tereza <bisharyant>
2024-05-28 19:24:39 +0400

    add cloudfront module

--- main.tf
+++ main.tf
@@ -4,6 +4,14 @@ module "s3" {
 	#lf_arn = module.lambda.lf_arn
 }
 
+module "cf_distribution" {
+	source = "./modules/cloudfront"
+	s3_bucket_name = module.s3.s3_bucket_name
+	origin_domain_name = module.s3.domain_name
+	origin_id = module.s3.origin_id
+	s3_bucket_arn = module.s3.bucket_arn
+}
+
 /*
 module "lambda" {
 	source = "./modules/lambda_function"
