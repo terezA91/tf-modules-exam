f5242dc6699b5c25e78838285ce522446bcf7f3f
Tereza <bisharyant>
2024-05-28 20:23:41 +0400

    c3

--- modules/cloudfront/config.tf
+++ modules/cloudfront/config.tf
@@ -1,18 +1,3 @@
 provider "aws" {
   region = "eu-north-1"
 }
-
-terraform {
-  required_version = ">= 1.0"
-
-  backend "local" {
-    path = "~/tf-modules-exam/modules/cloudfront/terraform.tfstate"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.62"
-    }
-  }
-}
