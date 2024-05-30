43a54441b135ea3b65bd174560b43b0fbacd1041
Tereza <bisharyant>
2024-05-30 14:03:20 +0400

    c71

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -23,13 +23,6 @@ resource "aws_s3_object" "ob" {
 	server_side_encryption = "AES256"
 }
 
-resource "null_resource" "wait_for_lambda_trigger" {
-  depends_on   = [aws_s3_object.ob]
-  provisioner "local-exec" {
-    command = "sleep 1m"
-  }
-}
-
 resource "aws_s3_bucket_public_access_block" "exam" {
 	bucket = aws_s3_bucket.b1.id
 	block_public_acls = var.s3_block_public_acls
