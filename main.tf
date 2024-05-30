445ed00016c55a3f1d4a3c9be9b1ee884321a1a0
Tereza <bisharyant>
2024-05-30 16:46:09 +0400

    c84

--- main.tf
+++ main.tf
@@ -1,24 +1,25 @@
 module "s3" {
 	source = "./modules/s3"
-
+/*lambda section
   trigger_lambda = true
 	lf_arn = module.lambda.lf_arn
 	lf_permission = module.lambda.lf_permission
-	
+*/
+		
 }
 
-module "lambda" {
-  source = "./modules/lambda_function"
-  bucket_arn = module.s3.bucket_arn
-}
-
-/*
 module "cf_distribution" {
   source = "./modules/cloudfront"
   s3_bucket_name = module.s3.s3_bucket_name
   origin_domain_name = module.s3.domain_name
   origin_id = module.s3.origin_id
   s3_bucket_arn = module.s3.bucket_arn
+}
+
+/*
+module "lambda" {
+  source = "./modules/lambda_function"
+  bucket_arn = module.s3.bucket_arn
 }
 
 module "vpc" {
