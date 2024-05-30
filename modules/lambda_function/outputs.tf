1f21d26d3d5058d1bf7fe5a97b4fbd67b0def39f
Tereza <bisharyant>
2024-05-30 11:03:08 +0400

    c61

--- modules/lambda_function/outputs.tf
+++ modules/lambda_function/outputs.tf
@@ -4,6 +4,6 @@ output "lf_arn" {
 }
 
 output "lf_permission" {
-    value = aws_lambda_permission.alp.source_arn
+    value = aws_lambda_permission.alp
     description = "Name of the <aws_lambda_permission> resource"
 }
