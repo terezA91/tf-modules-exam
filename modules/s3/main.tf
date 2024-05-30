24ba1bf126cebbf066fc6eca6eb51b1416c1d897
Tereza <bisharyant>
2024-05-30 06:13:09 +0400

    c41

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -17,7 +17,7 @@ resource "aws_s3_bucket" "b1" {
 resource "aws_s3_object" "ob" {
 	#depends_on = [aws_s3_bucket.b1]
   bucket = aws_s3_bucket.b1.bucket
-  source = var.object_source
+  source = "${path.module}/${var.object_source}"
   key = var.object_name
 	content_type = var.as_website == true ? "text/html" : "image/jpeg"
 	server_side_encryption = "AES256"
